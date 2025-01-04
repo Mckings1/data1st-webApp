d40a6fa3af31e3caaf86c94752a21949e711afe5
oluwasegun michael <alabioluwasegun8@gmail.com>
2025-01-04 15:26:49 +0100

    componentsandpagessetup

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import Header from "../Components/Header";
+import Selectnetwork from "../Components/Selectnetwork";
+import Howweworkit from "../Components/Howweworkit";
+import Testimonialreview from "../Components/Testimonialreview";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Selectnetwork />
+      <Howweworkit />
+      <Testimonialreview />
+    </div>
+  );
+};
+
+export default Home;
